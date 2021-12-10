09c2146cc4b2511eebed65092dfd158278a81ecd
Fran Tena <tarod.net@gmail.com>
2021-12-10 13:31:41 +0100

    first examples

---
+++ basic-server/app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// The createServer() method of http creates a new HTTP server and returns it.
+const server = http.createServer((req, res) => {
+  // We set the statusCode property to 200, to indicate a successful response.
+  res.statusCode = 200;
+  // We set the Content-Type header:
+  res.setHeader('Content-Type', 'text/plain');
+  // We set the Content-Type header:
+  res.end('Hello World');
+});
+
+// When the server is ready, the callback function is called, in this case informing us that the server is running.
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
