f4bf5876ee6f5519c591115fbfc6d3b8a03a120d
Fran Tena <tarod.net@gmail.com>
2021-12-15 17:21:59 +0100

    use environment variables

--- basic-server/app.js
+++ basic-server/app.js
@@ -1,7 +1,9 @@
+require('dotenv').config(); // read env vars
+
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = process.env.HOST
+const port = process.env.PORT
 
 // The createServer() method of http creates a new HTTP server and returns it.
 const server = http.createServer((req, res) => {
