6e26699865ce2df8b66eed829c5e986e4242a691
Fran Tena <tarod.net@gmail.com>
2022-01-08 15:35:07 +0100

    new sample: using export / import

---
+++ expose-functionality/app.js
@@ -0,0 +1,6 @@
+const items = require('./items')
+const car = items.car
+console.log(car);
+
+console.log(items.add(4, 4));
+console.log(items.substract(4, 8));
---
+++ expose-functionality/items.js
@@ -0,0 +1,16 @@
+// items.js
+const car = {
+	  brand: 'Ford',
+	  model: 'Fiesta'
+}
+
+function add(x, y) {
+	  return x + y;
+}
+  
+function substract(x, y) {
+	  return x - y;
+}
+
+module.exports = {car, add, substract}
+
