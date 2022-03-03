5758938cfe8a4660fc4a88c7ef51708e89ed21c3
Fran Tena <tarod.net@gmail.com>
2022-03-03 20:24:39 +0100

    add explanations

--- basic-get-post/app-get-standard.js
+++ basic-get-post/app-get-standard.js
@@ -6,12 +6,18 @@ const options = {
 	method: 'GET'
 }
 
+/* https.request() returns an instance of the http.ClientRequest class.
+ * The ClientRequest instance is a writable stream.
+ * Streams can be readable, writable, or both. All streams are instances of EventEmitter.
+ * https://nodejs.org/api/stream.html#stream
+ * https://nodejs.org/api/events.html#class-eventemitter
+ */
 const req = https.request(options, res => {
 	console.log(`statusCode: ${res.statusCode}`)
 
 	res.on('data', d => {
 		console.error("data!")
-		process.stdout.write(d)
+		// process.stdout.write(d)
 	})
 })
 
--- event-emitter/app.js
+++ event-emitter/app.js
@@ -1,4 +1,5 @@
 // + info @ https://nodejs.dev/learn/the-nodejs-event-emitter
+// .on() is exactly the same as .addListener() in the EventEmitter object (source: https://stackoverflow.com/questions/29861608/)
 
 const EventEmitter = require('events')
 const eventEmitter = new EventEmitter()
