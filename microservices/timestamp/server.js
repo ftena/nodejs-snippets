c919bc158dcf27b22a2a1d10276c04f35f9783c8
Fran Tena <tarod.net@gmail.com>
2022-04-15 15:25:07 +0200

    update packages

--- microservices/timestamp/server.js
+++ microservices/timestamp/server.js
@@ -52,6 +52,6 @@ app.get('/api/:date?', function(req, res, next) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
