b6db40fd38e8e065d4135166ae8bcda95d3f822c
Fran Tena <tarod.net@gmail.com>
2022-04-16 14:51:56 +0200

    update server

--- microservices/exercise-tracker/server.js
+++ microservices/exercise-tracker/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const myApp = require('./myApp');
-
+var bodyParser = require('body-parser'); // required to parse POST requests
+var mongoose = require("mongoose");
 require('dotenv').config()
 
 app.use(cors())
@@ -11,6 +11,55 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+/* Use body-parser to parse post requests */
+app.use(bodyParser.urlencoded({ extended: false}))
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+
+// Create a user schema called userSchema
+let userSchema = new Schema({
+  username: {type: String, required: true},
+  age: Number,  
+  favoriteFoods: [String]
+});
+
+// Create a model called User from the userSchema
+let User = mongoose.model("User", userSchema);
+
+// Exercise
+let exerciseSchema = new Schema({
+  username: {type: String, required: true},
+  description: String,  
+  duration: Number,
+  date: Date
+});
+let Exercise = mongoose.model("Exercise", exerciseSchema);
+
+// Log
+let logSchema = new Schema({
+  username: {type: String, required: true},
+  count: Number,  
+  log: [{description: String,  
+  duration: Number,
+  date: Date}]
+});
+let Log = mongoose.model("Log", logSchema);
+
+// Post api/users
+app.post('/api/users', function(req, res)
+{
+  console.log(req.body)
+  res.end()  
+})
+
+// Post api/users
+app.post('/api/users/:_id/exercises', function(req, res)
+{
+  console.log(req.body)
+  res.end()  
+})
 
 
 
