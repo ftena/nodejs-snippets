37977630f0010b2c15cae2cfb60c7b40dc1265c0
Fran Tena <tarod.net@gmail.com>
2022-04-28 12:00:09 +0000

    added post request

--- microservices/file-metadata/server.js
+++ microservices/file-metadata/server.js
@@ -11,8 +11,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+/* Get Data from POST Requests */
+app.post('/api/fileanalyse', function(req, res) {
+  console.log("params: " + req.params)
+  console.log("body: " + req.body)
+  
+  res.end()
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
