0dddbf781534e3afd2361ad1e3f797221ae90fdc
Fran Tena <tarod.net@gmail.com>
2022-04-28 15:04:48 +0000

    added code to post method

--- microservices/file-metadata/server.js
+++ microservices/file-metadata/server.js
@@ -1,25 +1,32 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer')
+const cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const upload = multer()
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 /* Get Data from POST Requests */
-app.post('/api/fileanalyse', function(req, res) {
-  console.log("params: " + req.params)
-  console.log("body: " + req.body)
-  
-  res.end()
+// It is important that you use the name field value from the form in your upload function. 
+// more info @ https://www.npmjs.com/package/multer
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+  console.log(req.file)
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
